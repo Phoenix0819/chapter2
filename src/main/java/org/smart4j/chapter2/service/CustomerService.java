2b76902ad82d8b93f45767b0ea98428d73d699f4
Phoenix0819 <1436762911@qq.com>
2016-12-22 17:07:32 +0800

    modify methods in CustomerServiceText
    Signed-off-by: Phoenix0819 <1436762911@qq.com>

---
+++ src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -0,0 +1,36 @@
+package org.smart4j.chapter2.service;
+
+import org.smart4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lenovo on 2016-12-22.
+ */
+public class CustomerService {
+    public List<Customer> getCustomerList (String keyword){
+        //TODO
+        return null;
+    }
+
+    public  Customer getCustomer(long id){
+        //TODO
+        return null;
+    }
+
+    public boolean createCustomer(Map<String,Object> fieldMap){
+        //TODO
+        return false;
+    }
+
+    public boolean updateCustomer(long id,Map<String,Object> fieldMap){
+        //TODO
+        return false;
+    }
+
+    public boolean deleteCustomer(long id){
+        //TODO
+        return false;
+    }
+}
--- src/test/java/org/smart4j/chapter2/test/CustomerServiceTest.java
+++ src/test/java/org/smart4j/chapter2/test/CustomerServiceTest.java
@@ -1,10 +1,12 @@
 package org.smart4j.chapter2.test;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.smart4j.chapter2.model.Customer;
 import org.smart4j.chapter2.service.CustomerService;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,27 +26,41 @@ public class CustomerServiceTest {
 
     @Test
     public void getCustomerListTest (){
-        //TODO
+        List<Customer> customerList=customerService.getCustomerList(null);
+        Assert.assertEquals(2,customerList.size());
     }
 
     @Test
     public  void getCustomerTest(){
-        //TODO
+        long id=1;
+        Customer customer=customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
     }
 
     @Test
     public void createCustomerTest(){
-        //TODO
+        Map<String,Object> fieldMap=new HashMap<String, Object>();
+        fieldMap.put("name","customer100");
+        fieldMap.put("contact","John");
+        fieldMap.put("telephone","13512345678");
+        boolean result =customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void updateCustomerTest(){
-        //TODO
+        long id=1;
+        Map<String,Object> fieldMap=new HashMap<String, Object>();
+        fieldMap.put("contract","Eric");
+        boolean result=customerService.updateCustomer(id,fieldMap);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void deleteCustomerTest(){
-        //TODO
+        long id=1;
+        boolean result=customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
     }
 
 }
